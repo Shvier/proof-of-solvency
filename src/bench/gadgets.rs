8be935435b9521a69ebc2d96c9b3566ed56b55f5
Shvier <Shvier1894@gmail.com>
2024-04-20 15:27:08 -0400

    add gadget to generate csv report

--- src/bench/gadgets.rs
+++ src/bench/gadgets.rs
@@ -1,9 +1,9 @@
-use std::{fs::File, io::{BufWriter, Write}};
+use std::{fs::{self, File}, io::{BufWriter, Write}};
 
 use ark_std::rand::{self, Rng};
 use csv::Writer;
 
-use super::BenchConfig;
+use super::{BenchConfig, CSVRecord};
 
 #[test]
 fn generate_balances() {
@@ -33,4 +33,73 @@ fn generate_config() {
     let mut writer = BufWriter::new(file);
     serde_json::to_writer(&mut writer, &configs).expect("Failed to serialize config json");
     writer.flush().expect("Failed to write config json file");
+}
+
+#[test]
+fn generate_csv_report() {
+    let prefix = &mut vec![];
+    let records = &mut vec![];
+    _generate_csv_report("./bench_data", 0, &vec!["users", "bits", "groups"], prefix, records);
+    println!("{:?}", records);
+    let path = "./bench_data/report.csv";
+    let mut wtr = Writer::from_path(path).expect("Failed to create file");
+    for record in records {
+        wtr.serialize(record).expect("Failed to serialize");
+    }
+    wtr.flush().expect("Failed to write");
+}
+
+#[cfg(test)]
+fn _generate_csv_report(
+    path: &str,
+    depth: usize,
+    levels: &Vec<&str>,
+    prefix: &mut Vec<usize>,
+    records: &mut Vec<CSVRecord>,
+) {
+    use std::io::Read;
+
+    use crate::bench::PoLReport;
+
+    if depth == levels.len() {
+        assert_eq!(prefix.len(), levels.len());
+        let paths = fs::read_dir(path).unwrap();
+        for path in paths {
+            if let Some(path) = path.ok() {
+                assert!(path.path().is_file());
+                let file_name = path.file_name().into_string().unwrap();
+                let mut split = file_name.split(".json");
+                let timestamp = split.next().unwrap();
+                let mut file = File::open(path.path()).unwrap();
+                let mut buffer = String::new();
+                file.read_to_string(&mut buffer).unwrap();
+                let report: PoLReport = serde_json::from_str(&buffer).unwrap();
+                let record = CSVRecord {
+                    num_of_users: prefix[0],
+                    num_of_bits: prefix[1],
+                    num_of_groups: prefix[2],
+                    interpolation_time: report.interpolation_time,
+                    proving_time: report.proving_time,
+                    verifying_time: report.verifying_time,
+                    proof_size: report.proof_size,
+                    timestamp: timestamp.to_string(),
+                };
+                records.push(record);
+            }
+        }
+        return;
+    }
+    let paths = fs::read_dir(path).unwrap();
+    for path in paths {
+        if let Some(path) = path.ok() {
+            if !path.path().is_dir() { continue; }
+            let path_str = path.path().into_os_string().into_string().unwrap().clone();
+            let folder = path.file_name().into_string().unwrap();
+            let mut split = folder.split(levels[depth]);
+            let pre = split.next().unwrap();
+            prefix.push(pre.parse::<usize>().unwrap());
+            _generate_csv_report(&path_str, depth + 1, &levels, prefix, records);
+            prefix.pop();
+        }
+    }
 }
--- src/bench/mod.rs
+++ src/bench/mod.rs
@@ -10,10 +10,22 @@ pub struct BenchConfig {
     pub num_of_groups: usize,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct PoLReport {
     pub interpolation_time: String,
     pub proving_time: String,
     pub verifying_time: String,
     pub proof_size: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CSVRecord {
+    pub num_of_users: usize,
+    pub num_of_bits: usize,
+    pub num_of_groups: usize,
+    pub interpolation_time: String,
+    pub proving_time: String,
+    pub verifying_time: String,
+    pub proof_size: String,
+    pub timestamp: String,
 }
