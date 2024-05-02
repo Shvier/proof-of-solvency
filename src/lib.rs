b27474ed03a47e078c5eaae53f07e6b968a6980c
Shvier <Shvier1894@gmail.com>
2024-05-02 10:28:47 -0400

    move pol from main to main_pol

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod proof_of_assets;
 pub mod utils;
 pub mod types;
 pub mod benchmark;
+pub mod main_pol;
 
 #[cfg(test)]
 mod test_utils;
--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,60 @@
 use std::{
-    env, fs::{self, File}, io::{BufWriter, Read, Write}, path::Path, time::{Duration, Instant}
+    env, io, path::Path
 };
 
-use ark_poly::domain::EvaluationDomain;
-use ark_std::{test_rng, UniformRand};
-
-use csv::ReaderBuilder;
-use proof_of_solvency::{
-    benchmark::{BenchConfig, PoLReport},
-    proof_of_liability::{prover::Prover, verifier::Verifier},
-    types::BlsScalarField,
-};
+use proof_of_solvency::
+    main_pol::run_pol
+;
 
 fn main() {
+    println!("Operation:");
+    println!("1. Run Proof-of-Liability");
+    println!("2. Run whole Proof-of-Assets");
+    println!("3. Run precomputation for Proof-of-Assets");
+    println!("4. Run Proof-of-Assets");
+    println!("5. Exit");
+
+    println!("Enter your choice:");
+
+    let mut choice = String::new();
+
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+
+    let choice: u32 = match choice.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a valid number!");
+            return;
+        }
+    };
+
     let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    let output_dir = &args[2];
-    assert!(Path::new(file_path).exists());
-    run_pol(file_path.to_string(), output_dir.to_string());
-}
-
-fn run_pol(bal_path: String, output_dir: String) {
-    let (configs, balances) = read_config(bal_path);
-    for config in configs {
-        let dir = format!(
-            "{}/{}users/{}bits/{}groups",
-            output_dir, config.num_of_users, config.num_of_bits, config.num_of_groups
-        );
-        let _ = fs::create_dir_all(dir.clone());
-        let bals = balances[0..config.num_of_users].to_vec();
-        let (proof_size, time1, time2, time3) = _run_pol(&config, &bals);
-        let report = PoLReport {
-            interpolation_time: format!("{:.2?}", time1.as_millis()),
-            proving_time: format!("{:.2?}", time2.as_millis()),
-            verifying_time: format!("{:.2?}", time3.as_millis()),
-            proof_size: format!("{}", proof_size / 1000),
-        };
-        let json_path =
-            dir.clone() + &format!("/{}.json", chrono::offset::Local::now()).replace(":", "-");
-        let file = File::create(json_path).unwrap();
-        let mut writer = BufWriter::new(file);
-        serde_json::to_writer(&mut writer, &report).unwrap();
-        writer.flush().unwrap();
-    }
-}
-
-fn _run_pol(config: &BenchConfig, balances: &Vec<u64>) -> (usize, Duration, Duration, Duration) {
-    let group_size: usize = config.num_of_users / config.num_of_groups;
-    let max_degree = group_size * 2;
-    let prover = Prover::setup(&balances, group_size, max_degree);
-    let rng = &mut test_rng();
-    let gamma = BlsScalarField::rand(rng);
-    match config.num_of_groups {
+    
+    match choice {
         1 => {
-            let now = Instant::now();
-            let (inters, comms, rands) = prover.run(config.num_of_bits, gamma, rng);
-            let elapsed1 = now.elapsed();
-            let taus = inters.iter().map(| inter | inter.domain.sample_element_outside_domain(rng)).collect();
-            let now = Instant::now();
-            let (proof, _) = prover.generate_proof(&inters, &comms, &rands, &taus, rng);
-            let proof_size = proof.deep_size();
-            let elapsed2 = now.elapsed();
-            let now = Instant::now();
-            Verifier::validate_liability_proof(&prover.vk, proof.clone(), &taus, gamma, rng);
-            let elapsed3 = now.elapsed();
-            (proof_size, elapsed1, elapsed2, elapsed3)
+            println!("You chose Option 1");
+            let bal_path = &args[1];
+            let output_dir = &args[2];
+            assert!(Path::new(bal_path).exists());
+            run_pol(bal_path.to_string(), output_dir.to_string());
         }
-        _ => {
-            let now = Instant::now();
-            let (inters, comms, rands) = prover.concurrent_run(config.num_of_bits, gamma);
-            let elapsed1 = now.elapsed();
-            let taus = inters
-                .iter()
-                .map(|inter| inter.domain.sample_element_outside_domain(rng))
-                .collect();
-            let now = Instant::now();
-            let (proof, _) = prover.concurrent_generate_proof(&inters, &comms, &rands, &taus);
-            let proof_size = proof.deep_size();
-            let elapsed2 = now.elapsed();
-            let now = Instant::now();
-            Verifier::validate_liability_proof(&prover.vk, proof, &taus, gamma, rng);
-            let elapsed3 = now.elapsed();
-            (proof_size, elapsed1, elapsed2, elapsed3)
+        2 => {
+            println!("You chose Option 2");
         }
+        3 => {
+            println!("You chose Option 3");
+        }
+        4 => {
+            println!("You chose Option 4");
+        }
+        5 => {
+            println!("Exiting...");
+            return;
+        }
+        _ => println!("Invalid choice!"),
     }
-}
-
-fn read_config(bal_path: String) -> (Vec<BenchConfig>, Vec<u64>) {
-    let mut file = File::open("./bench_data/proof_of_liability/config.json").unwrap();
-    let mut buffer = String::new();
-    file.read_to_string(&mut buffer).unwrap();
-    let configs: Vec<BenchConfig> = serde_json::from_str(&buffer).unwrap();
-
-    let file = File::open(bal_path).unwrap();
-    let mut reader = ReaderBuilder::new().has_headers(false).from_reader(file);
-    let mut balances = Vec::<u64>::new();
-    for result in reader.deserialize() {
-        let record: u64 = result.unwrap();
-        balances.push(record);
-    }
-    (configs, balances)
 }
 
 // fn to_disk<T: Sized>(path: &str, data: &T) {
