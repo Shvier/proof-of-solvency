7633f686d4c2befd302135ee02f67b07ea323f5a
Shvier <Shvier1894@gmail.com>
2024-05-03 13:44:09 -0400

    set limit for y-axis of verifying time

--- visualize_poa.py
+++ visualize_poa.py
@@ -27,18 +27,24 @@ def show_post_precomputing():
     proving_time_df = df['post_proving_time']
     verifying_time_df = df['post_verifying_proof_time']
     validating_balance_df = df['post_validating_balance_time']
+
     fig, axs = plt.subplots(1, 3, figsize=(18, 6))
+
     axs[0].plot(keys_df, proving_time_df)
     axs[0].set_xlabel('# of Keys')
     axs[0].set_ylabel('Proving Time (ms)')
+
+    axs[1].set_ylim([1, 10])
     axs[1].plot(keys_df, verifying_time_df)
     axs[1].set_xlabel('# of Keys')
     axs[1].set_ylabel('Verifying Proof Time (ms)')
+
     axs[2].plot(keys_df, validating_balance_df)
     axs[2].set_xlabel('# of Keys')
     axs[2].set_ylabel('Validating Balance Time (ms)')
+
     fig.suptitle('Post-Precomputing Stage')
     plt.show()
 
-# show_precomputing()
+show_precomputing()
 show_post_precomputing()
